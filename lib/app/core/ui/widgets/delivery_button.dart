52ecc258a2c45d3178f6909380a67ab9c2c9375d
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-02-03 16:11:53 -0300

    FEAT: botão customizado a ser usado em todo app;

---
+++ lib/app/core/ui/widgets/delivery_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class DeliveryButton extends StatelessWidget {
+  final String label;
+  final VoidCallback? onPressed;
+  final double? width;
+  final double? height;
+
+  const DeliveryButton(
+      {Key? key,
+      required this.label,
+      this.onPressed,
+      this.width,
+      this.height = 50})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(label),
+    );
+  }
+}
