95875e89a13c20db0c04be6ddf20cde532ce9be8
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-02-03 16:15:28 -0300

    FEAT: singleton para consumo de estilos no app;

---
+++ lib/app/core/ui/styles/app_styles.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'text_styles.dart';
+
+class AppStyles {
+  static AppStyles? _instance;
+
+  AppStyles._();
+
+  static AppStyles get instance {
+    _instance ??= AppStyles._();
+    return _instance!;
+  }
+
+  ButtonStyle get primaryButton => ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(7),
+        ),
+        textStyle: TextStyles.instance.textButtonLabel,
+      );
+}
+
+extension AppStylesExtensions on BuildContext {
+  AppStyles get appStyles => AppStyles.instance;
+}
