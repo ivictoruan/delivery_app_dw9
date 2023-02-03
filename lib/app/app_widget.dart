ce6bf0330d6245e3c296bd0a5d06c27e598232c0
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-02-03 16:19:03 -0300

    REFACTOR: add tema ao app;

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,17 +1,19 @@
-import 'package:delivery_app_dw9/app/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
+import 'core/ui/theme/theme_config.dart';
+import 'pages/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       title: "Café com Frutas App",
+      theme: ThemeConfig.theme,
       routes: {
         '/': (context) => const SplashPage(),
-      }
+      },
     );
   }
 }
