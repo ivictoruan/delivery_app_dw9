bb4f3522d7d8a1025ae3517fc21940917789e5de
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-02-05 20:01:07 -0300

    REFACTOR: add assets e botão(navegando p/ home);

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,14 +1,55 @@
+import 'package:delivery_app_dw9/app/core/ui/helpers/size_extensions.dart';
+import 'package:delivery_app_dw9/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
 
-  const SplashPage({ Key? key }) : super(key: key);
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Splash'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    String startLogoPath = "assets/images/logo.png";
+    String startHamburgerPath = "assets/images/lanche.png";
+    return Scaffold(
+      body: ColoredBox(
+        color: const Color(0XFF140E0E),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: context.screenWidth,
+                child: Image.asset(
+                  startHamburgerPath,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Center(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: context.percentageHeight(0.2),
+                  ),
+                  Image.asset(
+                    startLogoPath,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(
+                    height: context.percentageHeight(0.1),
+                  ),
+                  DeliveryButton(
+                    label: "ACESSAR",
+                    width: context.percentageWidth(.6),
+                    height: 40,
+                    onPressed: () =>
+                        Navigator.of(context).popAndPushNamed("/home"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
