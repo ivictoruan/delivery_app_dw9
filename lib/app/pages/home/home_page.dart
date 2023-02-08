f38c376b0bf931530921e2c292632ff15402a202
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-02-08 18:34:27 -0300

    REFACTOR: adicionando sacola/botão;

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../core/ui/base_state/base_state.dart';
-import '../../core/widgets/delivery_app_bar.dart';
+import '../../core/ui/widgets/delivery_app_bar.dart';
 import 'home_controller.dart';
 import 'home_state.dart';
 import 'widgets/product_tile_widget.dart';
+import 'widgets/shopping_bag_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -25,38 +26,57 @@ class _HomePageState extends BaseState<HomePage, HomeController> {
     return Scaffold(
       appBar: DeliveryAppBar(),
       body: BlocConsumer<HomeController, HomeState>(
-          listener: (context, state) {
-            state.status.matchAny(
-                any: hideLoader,
-                loading: () => showLoader(),
-                error: () {
-                  hideLoader();
-                  showError(state.errorMessage ?? "Erro não informado!");
-                });
-          },
-          buildWhen: (previous, current) => current.status.matchAny(
-                any: () => false,
-                initial: () => true,
-                loaded: () => true,
-              ),
-          builder: (context, state) {
-            final products = state.products;
-            return Column(
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: products.length,
-                    itemBuilder: (context, index) => Padding(
+        listener: (context, state) {
+          state.status.matchAny(
+              any: hideLoader,
+              loading: () => showLoader(),
+              error: () {
+                hideLoader();
+                showError(state.errorMessage ?? "Erro não informado!");
+              });
+        },
+        buildWhen: (previous, current) => current.status.matchAny(
+          any: () => false,
+          initial: () => true,
+          loaded: () => true,
+        ),
+        builder: (context, state) {
+          return Column(
+            children: [
+              // Text(
+              //   state.shoppingBag.length.toString(),
+              //   style: const TextStyle(color: Colors.black),
+              // ),
+
+              Expanded(
+                child: ListView.builder(
+                  itemCount: state.products.length,
+                  itemBuilder: (context, index) {
+                    final product = state.products[index];
+                    final orders = state.shoppingBag
+                        .where((order) => order.product == product);
+
+                    return Padding(
                       padding: const EdgeInsets.only(bottom: 15),
                       child: ProductTileWidget(
-                        product: products[index],
+                        product: product,
+                        orderProduct: orders.isNotEmpty ? orders.first : null,
                       ),
-                    ),
-                  ),
+                    );
+                  },
                 ),
-              ],
-            );
-          }),
+              ),
+
+              Visibility(
+                visible: state.shoppingBag.isNotEmpty,
+                child: ShoppingBagWidget(
+                  bag: state.shoppingBag,
+                ),
+              )
+            ],
+          );
+        },
+      ),
     );
   }
 }
