9b2c04732ba180bbfbaa23a82fd138b7ef159850
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-02-23 19:10:13 -0300

    REFACTOR: add copyWith;

--- lib/app/dto/order_product_dto.dart
+++ lib/app/dto/order_product_dto.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import '../models/product_model.dart';
 
 class OrderProductDto {
@@ -9,4 +10,14 @@ class OrderProductDto {
   });
 
   double get totalPrice => product.price * amount;
+
+  OrderProductDto copyWith({
+    ProductModel? product,
+    int? amount,
+  }) {
+    return OrderProductDto(
+      product: product ?? this.product,
+      amount: amount ?? this.amount,
+    );
+  }
 }
