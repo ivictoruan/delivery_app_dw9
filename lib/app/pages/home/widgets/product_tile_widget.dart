8e634641f2571445f2b106c4983c9aa16b85486b
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-02-28 11:17:59 -0300

    REFACTOR: usando widget p/ carregamento de imagens

--- lib/app/pages/home/widgets/product_tile_widget.dart
+++ lib/app/pages/home/widgets/product_tile_widget.dart
@@ -1,10 +1,10 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:delivery_app_dw9/app/core/extensions/formatter_extension.dart';
 import 'package:delivery_app_dw9/app/core/ui/styles/colors_app.dart';
 import 'package:delivery_app_dw9/app/core/ui/styles/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../core/ui/widgets/show_product_image_widget.dart';
 import '../../../dto/order_product_dto.dart';
 import '../../../models/product_model.dart';
 import '../home_controller.dart';
@@ -66,45 +66,7 @@ class ProductTileWidget extends StatelessWidget {
                 ],
               ),
             ),
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: Colors.grey[100],
-                borderRadius: BorderRadius.circular(7),
-              ),
-              child: Image(
-                image: CachedNetworkImageProvider(
-                  product.image,
-                  maxWidth: 160,
-                  maxHeight: 120,
-                ),
-                fit: BoxFit.cover,
-                loadingBuilder: (context, child, loadingProgress) {
-                  if (loadingProgress == null) {
-                    return child;
-                  }
-                  return Center(
-                    child: CircularProgressIndicator(
-                      color: context.colors.secondary,
-                      strokeWidth: 2,
-                    ),
-                  );
-                },
-              ),
-            ),
-            // FadeInImage.assetNetwork(
-            //   image: product.image,
-            //   placeholder: "assets/images/loading.gif",
-            //   width: 100,
-            //   height: 100,
-            //   imageErrorBuilder: (context, error, stackTrace) {
-            //     return Image.asset(
-            //       'assets/images/lanche.png',
-            //       fit: BoxFit.fitWidth,
-            //     );
-            //   },
-            // ),
+            ShowProductImageWidget(urlImage: product.image),
           ],
         ),
       ),
