b911831d940854fabe4c993c8d638c4def87e06e
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-02-28 11:25:54 -0300

    REFACTOR: add contexto de nav/rota de finalização;

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,32 +1,40 @@
 import 'package:flutter/material.dart';
 
+import 'core/global/global_context.dart';
 import 'core/provider/application_binding.dart';
 import 'core/ui/theme/theme_config.dart';
 import 'pages/auth/login/login_router.dart';
 import 'pages/auth/register/register_router.dart';
 import 'pages/home/home_router.dart';
+import 'pages/order/order_completed_page.dart';
 import 'pages/order/order_router.dart';
 import 'pages/product_detail/product_detail_router.dart';
 import 'pages/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
-  const AppWidget({Key? key}) : super(key: key);
+  final _navKey = GlobalKey<NavigatorState>();
+
+  AppWidget({super.key}) {
+    GlobalContext.instance.navigatorKey =
+        _navKey; // jogando _nav p/ global context
+  }
 
   @override
   Widget build(BuildContext context) {
-    // debugInvertOversizedImages = true;
     return ApplicationBinding(
       child: MaterialApp(
         title: "Café com Frutas App",
         theme: ThemeConfig.theme,
+        navigatorKey: _navKey,
         debugShowCheckedModeBanner: false,
         routes: {
           '/': (context) => const SplashPage(),
           '/home': (context) => HomeRouter.page,
           '/productDetail': (context) => ProductDetailRouter.page,
-          '/auth/login': (context) =>LoginRouter.page,
+          '/auth/login': (context) => LoginRouter.page,
           '/auth/register': (context) => RegisterRouter.page,
           '/order': (context) => OrderRouter.page,
+          '/order/completed': (context) => const OrderCompletedPage(),
         },
       ),
     );
