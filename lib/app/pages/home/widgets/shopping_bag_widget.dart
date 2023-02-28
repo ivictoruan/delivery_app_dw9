359d3891adba4c58ac55ccc5e42e1f3fe700c343
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-02-28 11:16:42 -0300

    REFACTOR: função ir para venda + botão alterado;

--- lib/app/pages/home/widgets/shopping_bag_widget.dart
+++ lib/app/pages/home/widgets/shopping_bag_widget.dart
@@ -1,36 +1,29 @@
 import 'package:delivery_app_dw9/app/core/extensions/formatter_extension.dart';
 import 'package:delivery_app_dw9/app/core/ui/helpers/size_extensions.dart';
-import 'package:delivery_app_dw9/app/core/ui/styles/colors_app.dart';
 import 'package:delivery_app_dw9/app/core/ui/styles/text_styles.dart';
 import 'package:delivery_app_dw9/app/dto/order_product_dto.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import '../home_controller.dart';
 
 class ShoppingBagWidget extends StatelessWidget {
   final List<OrderProductDto> bag;
 
   const ShoppingBagWidget({super.key, required this.bag});
 
-  Future<void> _goOrder(BuildContext context) async {
-    final navigator = Navigator.of(context);
-    final sp = await SharedPreferences.getInstance();
-    if (!sp.containsKey("accessToken")) {
-      final loginResult = await navigator.pushNamed("/auth/login");
-      bool notLogged = loginResult == null || loginResult == false;
-      if (notLogged) {
-        return; // [premature return]
-      }
-    }
-    await navigator.pushNamed("/order", arguments: bag);
-  }
-
   @override
   Widget build(BuildContext context) {
-    final totalBag = bag.fold<double>(0.0,
-        (totalValue, orderProduct) => totalValue += orderProduct.totalPrice);
+    final totalBag = bag
+        .fold<double>(0.0,
+            (totalValue, orderProduct) => totalValue + orderProduct.totalPrice)
+        .currencyPTBR;
+
     return Container(
       width: context.screenWidth,
       height: 90,
+      padding: const EdgeInsets.all(16),
       decoration: const BoxDecoration(
         color: Colors.white,
         boxShadow: [
@@ -45,52 +38,60 @@ class ShoppingBagWidget extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: InkWell(
-          onTap: () => _goOrder(context),
-          child: Container(
-            width: 343,
-            height: 48,
-            decoration: BoxDecoration(
-              color: context.colors.primary,
-              borderRadius: BorderRadius.circular(7),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Stack(
-                children: [
-                  const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Icon(
-                      Icons.shopping_cart_outlined,
-                      color: Colors.white,
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      "Ver sacola",
-                      style: context.textStyles.textExtraBold.copyWith(
-                        fontSize: 14,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: Text(
-                      totalBag.currencyPTBR,
-                      style: context.textStyles.textExtraBold.copyWith(
-                        fontSize: 11,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
+        child: ElevatedButton(
+          onPressed: () => _goToOrder(context),
+          child: Stack(
+            children: [
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Icon(
+                  Icons.shopping_cart_outlined,
+                  color: Colors.white,
+                ),
               ),
-            ),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "Ver sacola",
+                  style: context.textStyles.textExtraBold.copyWith(
+                    fontSize: 14,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  totalBag,
+                  style: context.textStyles.textExtraBold.copyWith(
+                    fontSize: 11,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
+  }
+
+  Future<void> _goToOrder(BuildContext context) async {
+    final navigator = Navigator.of(context);
+    final controller = context.read<HomeController>();
+    final sp = await SharedPreferences.getInstance();
+
+    if (!sp.containsKey("accessToken")) {
+      final loginResult = await navigator.pushNamed("/auth/login");
+      bool notLogged = loginResult == null || loginResult == false;
+      if (notLogged) {
+        return; // [premature return]
+      }
+    }
+    // envio para order
+    final updatedBag = await navigator
+        .pushNamed("/order", arguments: bag);
+
+    controller.updateBag((updatedBag ?? const <OrderProductDto>[]) as List<OrderProductDto>);
   }
 }
